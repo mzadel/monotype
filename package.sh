64bfdc0b42a8830d28b4aa6b8557b2ee7f9f94a1
Mark Zadel <zadel@music.mcgill.ca>
2012-10-09 12:37:36 -0400

    added package.sh for zipping up the image with a readme for distribution

---
+++ package.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# this script assumes that build.sh has been run
+
+MONOTYPEVERSION=monotype-$(date +%Y%m%d)-git$(git rev-parse --short HEAD)
+
+mkdir $MONOTYPEVERSION
+cp monotype.img $MONOTYPEVERSION
+cp usbkeyfiles/readme.txt $MONOTYPEVERSION
+zip -r $MONOTYPEVERSION.zip $MONOTYPEVERSION
+
